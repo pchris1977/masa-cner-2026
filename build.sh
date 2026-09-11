659a6a08cabdf7b4df8c36d88b60177956f28700
pchris1977 <cristy@asis.ro>
2026-09-11 19:09:45 +0300

    build.sh: poze inline in copia pentru artifact

--- build.sh
+++ build.sh
@@ -9,3 +9,12 @@ OUT="${1:-index.html}"
 if [ -n "$ARTIFACT" ]; then
   sed -e "s#__PDF_URL__#$B/Caiet_de_sarcini_Catering_CNER_Cluj.pdf#g" -e "s#__CAMIGO_URL__#$B/Oferta_Camigo_2026-2027.pdf#g" -e "s#__BSE_URL__#$B/Oferta_Black_Swan_2026-2027.pdf#g" -e "s#__VIBES_JPG__#$B/Oferta_Vibes_Bistro_2026-2027.jpeg#g" -e "s#__VIBES_XLSX__#$B/Oferta_Vibes_Bistro_meniu_2026-2027.xlsx#g" -e "s#__RAFINATO_URL__#$B/Oferta_Rafinato_Events_2026-2027.pdf#g" -e "s#__POZE__#$B/poze/#g" -e 's#__CAMIGO_GO__#Deschide ↗#g' template.html > "$ARTIFACT"
 fi
+# In copia pentru artifact, pozele se incorporeaza ca data URI (viewerul nu incarca imagini externe)
+if [ -n "$ARTIFACT" ]; then
+  for f in poze/*.jpeg; do
+    d="data:image/jpeg;base64,$(base64 -w0 "$f")"
+    printf '%s\n' "$d" > "$ARTIFACT.uri"
+    awk -v src="$B/$f" -v urifile="$ARTIFACT.uri" 'BEGIN{getline uri < urifile} { i=index($0, src); while (i>0) { $0 = substr($0,1,i-1) uri substr($0,i+length(src)); i=index($0, src) } print }' "$ARTIFACT" > "$ARTIFACT.tmp" && mv "$ARTIFACT.tmp" "$ARTIFACT"
+  done
+  rm -f "$ARTIFACT.uri"
+fi
